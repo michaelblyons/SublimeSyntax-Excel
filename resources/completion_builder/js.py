270586aacbda7ba0396a6a208ba0e3728b46b7d3
axemonk <axemonk667@gmail.com>
2025-02-08 18:06:30 -0600

    Consolidate comments into `main.py` and clean up

--- resources/completion_builder/js.py
+++ resources/completion_builder/js.py
@@ -1,23 +1,3 @@
-# Plan
-# 1. (DONE) Covert the function data to JSON, where each has:
-#       "func_name" (String)
-#       "req_param" (Array)
-#       "opt_param" (Array)
-#       "ellipsis" (Boolean)
-# 2. (DONE) Write code to build out completion strings for param-having functions
-# 
-# 3. Write code to build out completion strings for nullary functions
-# 
-# 4. Write code to match completion strings to descriptions, categories, and function names(?)
-# 
-# Last. Write code to build out entire .sublime-completions file.
-# 
-# NOTE: Not all ellipsis == True functions follow the same format, e.g.:
-#       1. COUNTA~value1,[value2],...
-#       2. COUNTIFS~criteria_range1,criteria1,...
-#       Before running this script, clean formulas that look like 2. to look like 1. using your best judgement and find using:
-#           [^\]],\.{3}
-
 import json
 import re
 
--- resources/completion_builder/main.py
+++ resources/completion_builder/main.py
@@ -1,8 +1,32 @@
-import subprocess
-import json
+# General operations performed by the scripts found in this directory:
+# 
+# 1. Covert the function data found in func_data_list.txt to JSON, where each has:
+#       "func_name" (String)
+#       "req_param" (Array)
+#       "opt_param" (Array)
+#       "ellipsis" (Boolean)
+#
+# 2. Build out completion strings for param-having functions
+# 
+# 4. Covert the function metadata found in func_ref.xlsx to JSON
+# 
+# 4. Match completion strings to descriptions, categories, and function names
+# 
+# 3. Build out completion strings for nullary functions
+# 
+# Last. Build out entire .sublime-completions file.
+# 
+# NOTE: Not all ellipsis == True functions follow the same format, e.g.:
+#       1. COUNTA~value1,[value2],...
+#       2. COUNTIFS~criteria_range1,criteria1,...
+#       Before running this script, clean formulas that look like 2. to look like 1. using your best judgement and find using:
+#           [^\]],\.{3}
 
 # TODO: Allow this entire directory to switch between application specific information like the `scope` found in this file and
 #       the file names used by all scripts instead of having to refactor a bunch of stuff every time.
+
+import subprocess
+import json
 
 scripts = ['js.py', 'cb.py', 'ld.py', 'mg.py']
 
