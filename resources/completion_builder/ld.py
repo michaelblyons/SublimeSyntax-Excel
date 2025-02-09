21a3ea82ca9b9bc34eac8365a40af0b59973ae7d
Michael Lyons <git@michael.lyo.nz>
2025-02-09 17:40:21 -0500

    Add argument parser to ld.py

--- resources/completion_builder/ld.py
+++ resources/completion_builder/ld.py
@@ -2,13 +2,39 @@
 
 import json
 import pandas as pd
-import sys
+from argparse import ArgumentParser
 
-app = sys.argv[1]
+from SpreadsheetFunction import APP_SUFFIXES, SpreadsheetFunction
 
-# Must use na_filter = False to prevent the func_name field of the NA() function's object being set to null.
-ref_file = pd.read_excel(f'{app}_funcs_ref.xlsx', sheet_name = 'Sheet1', na_filter = False).to_json(orient = 'records', indent = 4)
+def dump_function_args(sheet_app: str, write_json_file: bool = True):
 
-o = open(f'{app}_funcs_ref.json', 'w')
+    # Must use na_filter = False to prevent the func_name field of the NA() function's object being set to null.
+    ref_file = pd.read_excel(
+        f'{sheet_app}_funcs_ref.xlsx',
+        sheet_name='Sheet1',
+        na_filter=False
+    ).to_json(orient='records', indent=4)
 
-o.write(ref_file)
+    if write_json_file:
+        with open(f'{sheet_app}_funcs_ref.json', 'w') as o:
+            o.write(ref_file)
+
+    return ref_file
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        description='Dump spreadsheet function descriptions to JSON from Excel')
+    parser.add_argument(
+        '-s', '--spreadsheet-app',
+        choices=APP_SUFFIXES,
+        default='excel',
+        help='spreadsheet application')
+    parser.add_argument(
+        '-j', '--dump-json',
+        type=bool,
+        default=True,
+        help='dump JSON output to a file')
+    args = parser.parse_args()
+
+    dump_function_args(args.spreadsheet_app, args.dump_json)
